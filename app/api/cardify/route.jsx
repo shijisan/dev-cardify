47b252197740da957131bde49e072fdd5ec5973a
shijisan <cjsantos2411638@gmail>
2025-01-29 15:50:45 +0800

    testing sparticuz chromium

--- app/api/cardify/route.jsx
+++ app/api/cardify/route.jsx
@@ -1,9 +1,10 @@
-import { chromium } from 'playwright';
+import chromium from '@sparticuz/chromium';
 import { createCanvas, loadImage } from 'canvas';
 import { NextResponse } from 'next/server';
 import sharp from 'sharp';
 import ico from 'sharp-ico';
 import axios from 'axios';
+import playwright from 'playwright-core';
 
 export async function POST(req) {
 	const { url } = await req.json();
@@ -12,7 +13,6 @@ export async function POST(req) {
 		return NextResponse.json({ error: 'URL is required' }, { status: 400 });
 	}
 
-	// Variables to easily manipulate at the top
 	const canvasWidth = 500;
 	const canvasHeight = 500;
 	const iconSize = 100;
@@ -21,7 +21,11 @@ export async function POST(req) {
 
 	let browser;
 	try {
-		browser = await chromium.launch();
+		browser = await playwright.chromium.launch({
+			args: chromium.args,
+			executablePath: await chromium.executablePath(),
+			headless: true,
+		});
 		const page = await browser.newPage();
 		await page.goto(url, { waitUntil: 'networkidle', timeout: 30000 });
 
@@ -32,32 +36,27 @@ export async function POST(req) {
 
 		const mainColor = processColor(colors.navbarBackground) || processColor(colors.bodyBackgroundColor) || '#ffffff';
 		const headerColor = processColor(colors.headerText) || processColor(colors.bodyTextColor) || '#000000';
-		const anchorColor = processColor(colors.anchorText) ||  processColor(colors.bodyTextColor)  ||'#555555';
+		const anchorColor = processColor(colors.anchorText) || processColor(colors.bodyTextColor) || '#555555';
 
-		// Create canvas and set background
 		const canvas = createCanvas(canvasWidth, canvasHeight);
 		const ctx = canvas.getContext('2d');
 		ctx.fillStyle = mainColor;
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-		// Calculate text and image positions
 		const titleX = canvas.width / 2;
 		const titleY = 150;
 		const descriptionY = canvas.height - 150;
 		const iconX = (canvas.width - iconSize) / 2;
 		const iconY = (canvas.height - iconSize) / 2;
 
-		// Add title
 		ctx.fillStyle = headerColor;
 		ctx.font = titleFont;
 		ctx.textAlign = 'center';
 		wrapText(ctx, pageTitle, titleX, titleY, 460, 40, true);
 
-		// Process favicon and add logo image
 		const logoImage = await processFavicon(faviconUrl, iconSize);
 		ctx.drawImage(logoImage, iconX, iconY, iconSize, iconSize);
 
-		// Set description text color and add description
 		ctx.fillStyle = anchorColor;
 		ctx.font = descriptionFont;
 		wrapText(ctx, pageDescription, titleX, descriptionY, 460, 30);
@@ -73,12 +72,10 @@ export async function POST(req) {
 	}
 }
 
-// Function to extract meta description from the page
 async function getMetaDescription(page) {
 	return await page.$eval('meta[name="description"]', (meta) => meta?.content).catch(() => '');
 }
 
-// Function to extract colors from the page
 async function extractColors(page) {
 	return await page.evaluate(() => {
 		const navbar = document.querySelector('nav, header, .header, .navbar, #header, #navbar');
@@ -92,12 +89,11 @@ async function extractColors(page) {
 			headerText: header ? window.getComputedStyle(header).color : null,
 			anchorText: anchor ? window.getComputedStyle(anchor).color : null,
 			bodyBackgroundColor: bodyBackgroundColor || '#ffffff',
-			bodyTextColor: bodyTextColor || '##D3D3D3',
+			bodyTextColor: bodyTextColor || '#D3D3D3',
 		};
 	});
 }
 
-// Function to get favicon URL from the page
 async function getFaviconUrl(page) {
 	const faviconLinks = await page.evaluate(() => {
 		const links = document.querySelectorAll('link[rel*="icon"]');
@@ -117,7 +113,6 @@ async function getFaviconUrl(page) {
 		faviconLinks[0].href;
 }
 
-// Function to process favicon image
 async function processFavicon(faviconUrl, iconSize) {
 	const response = await axios({
 		method: 'get',
@@ -151,7 +146,6 @@ async function processFavicon(faviconUrl, iconSize) {
 	return loadImage(iconBuffer);
 }
 
-// Function to process color to hex
 function processColor(color) {
 	if (!color) return null;
 	if (color.startsWith('rgb')) {
@@ -162,26 +156,20 @@ function processColor(color) {
 	return color;
 }
 
-// Function to convert RGB to Hex
 function rgbToHex(r, g, b) {
 	return `#${[r, g, b].map(x => x.toString(16).padStart(2, '0')).join('').toUpperCase()}`;
 }
 
-// Function to wrap text within a specific width and truncate if necessary
 function wrapText(ctx, text, x, y, maxWidth, lineHeight, truncate = false) {
 	if (!text) return;
 	const words = text.split(' ');
 	let line = '';
 	let posY = y;
 
-	// Check if truncation is needed (only for title)
 	if (truncate && ctx.measureText(text).width > maxWidth) {
-		// Truncate and add ellipses
-		console.log("Title too long. Truncating...");
 		text = text.substring(0, Math.floor(text.length * maxWidth / ctx.measureText(text).width) - 3) + '...';
 	}
 
-	// Split the text into words and proceed with wrapping
 	const truncatedWords = text.split(' ');
 
 	for (const word of truncatedWords) {
